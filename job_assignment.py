bdeca191bad2a62a07dc6642b5c44282da272ecc
mphatic <theteqmaster@gmail.com>
2026-01-01 17:04:25 +0200

    Update job_assignment.py

--- job_assignment.py
+++ job_assignment.py
@@ -1,1 +1,5 @@
-
+print('hello world')
+#this is python project 
+#real life implementation of nqueens to solve real world problem
+#problem ,the schedule for maintanance at company X, is being managed using paper and is inefficient
+#proposed : use n queens algorithm to set areas to be maintained, creating an efficient schedule 
